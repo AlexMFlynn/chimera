7951d536f2090d8aae445a98082223317900e9e1
Alex Flynn <aflynn501@gmail.com>
2023-12-30 20:57:28 -0500

    feat: added chakra ui

--- apps/refero/pages/_app.tsx
+++ apps/refero/pages/_app.tsx
@@ -1,15 +1,19 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import './styles.css';
+import { ChakraProvider } from '@chakra-ui/react';
+import { ReactElement } from 'react';
 
-function CustomApp({ Component, pageProps }: AppProps) {
+function CustomApp({ Component, pageProps }: AppProps): ReactElement {
   return (
     <>
       <Head>
         <title>Welcome to refero!</title>
       </Head>
       <main className="app">
-        <Component {...pageProps} />
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
       </main>
     </>
   );
---
+++ apps/refero/pages/theme.ts
@@ -0,0 +1,12 @@
+import { extendTheme, type ThemeConfig } from '@chakra-ui/react';
+
+const config: ThemeConfig = {
+  initialColorMode: 'system',
+  useSystemColorMode: false
+};
+
+export const theme = extendTheme({
+  config
+});
+
+export default theme;
