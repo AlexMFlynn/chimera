6f8c271439846b45f51d6eb20574bef0d38f48ce
Alex Flynn <aflynn501@gmail.com>
2023-12-30 22:11:17 -0500

    feat: created picture card component

---
+++ apps/refero/components/pictureCard.tsx
@@ -0,0 +1,58 @@
+import {
+  Box,
+  Center,
+  Flex,
+  Heading,
+  Image,
+  Stack,
+  Text,
+  useColorModeValue
+} from '@chakra-ui/react';
+import { FC, PropsWithChildren } from 'react';
+
+export interface PicCardProps extends PropsWithChildren {
+  alt: string;
+  img: string;
+  heading: string;
+}
+
+export const PicCard: FC<PicCardProps> = ({ alt, children, heading, img }) => {
+  return (
+    <Flex py={6}>
+      <Box
+        maxW={'445px'}
+        w={'full'}
+        bg={useColorModeValue('white', 'gray.600')}
+        boxShadow={'2xl'}
+        rounded={'md'}
+        minH={'33em'}
+        p={6}
+        overflow={'hidden'}
+      >
+        <Box
+          h={'210px'}
+          bg={'gray.100'}
+          mt={-6}
+          mx={-6}
+          mb={6}
+          pos={'relative'}
+        >
+          <Image src={img} alt={alt} boxSize="full" objectFit="cover" />
+        </Box>
+        <Stack>
+          <Heading
+            color={useColorModeValue('gray.700', 'white')}
+            textAlign='center'
+            fontSize={'2xl'}
+            fontFamily={'body'}
+          >
+            {heading}
+          </Heading>
+          <Text color={useColorModeValue('gray.500', 'gray.200')} textAlign='center'>
+            {children}
+          </Text>
+        </Stack>
+      </Box>
+    </Flex>
+  );
+};
